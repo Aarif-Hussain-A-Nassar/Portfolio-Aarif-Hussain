38aba8aad886c2f7bcc6a7090f025d4af8ce0905
Aarif Hussain <aarifhuss70349@gmail.com>
2025-09-25 08:36:08 +0530

    Chnages

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,17 +1,5 @@
 import { Container } from "./styles";
 import VinayakSingh from "../../assets/VinayakSingh.webp";
-// import python from "../../assets/python.svg"
-// import java from "../../assets/java.svg"
-// import wordpress from "../../assets/wordpress.svg";
-// import shopify from "../../assets/shopify.svg";
-// import htmlIcon from "../../assets/html-icon.svg";
-// import cssIcon from "../../assets/css-icon.svg";
-// import jsIcon from "../../assets/js-icon.svg";
-// // import nodeIcon from "../../assets/node-icon.svg";
-// import reactIcon from "../../assets/react-icon.svg";
-// import typescriptIcon from "../../assets/typescript-icon.svg";
-// import vueIcon from "../../assets/vue-icon.svg";
-// import boostrapIcon from "../../assets/bootstrap-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
 export function About() {
@@ -19,105 +7,56 @@ export function About() {
     <Container id="about">
       <div className="about-text">
         <ScrollAnimation animateIn="fadeInLeft">
-          <h2>About me</h2>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
-          <p>
-            Hi there! I'm Vinayak, an enthusiastic Software Engineer skilled in web development, machine learning, and scalable software solutions
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-          <p>
-            I build responsive web applications, fine-tune ML models, and implement robust backend services.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
-          <p>
-            I also work with CMS platforms like WordPress and Shopify to streamline content management.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={400}>
-          <div className="education">
-            <h3>Education:</h3>
-            <h4> Master of Computer Applications (MCA)</h4>
-            <p>Vellore Institute of Technology, Chennai | July 2023 - Present</p>
-            <p>8.09 CGPA</p>
-          </div>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={550}>
-          <div className="experience">
-             <h3>Experience:</h3><h4>Software Developer</h4>
-            <p>KIWIS AND BROWNIES IT SOLUTIONS | October 2024 - Present</p>
-            <p>Bangalore, India</p>
-          </div>
+          <h2>About Me</h2>
         </ScrollAnimation>
 
-        {/* <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
-          <h3>Here are my main skills:</h3>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Aarif Hussain, a Software Engineer with 2+ years of
+            experience in front-end development and scalable knowledge in
+            backend technologies like .NET and APIs.
+          </p>
         </ScrollAnimation>
-        <div className="hard-skills">
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
-              <img src={python} alt="python" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.11 * 1000}>
-              <img src={java} alt="java" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
-              <img src={typescriptIcon} alt="Typescript" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
-              <img src={vueIcon} alt="Vue" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-              <img src={wordpress} alt="Wordpress" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
-              <img src={shopify} alt="shopify" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
-              <img src={htmlIcon} alt="Html" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={cssIcon} alt="Css" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.20 * 1000}>
-              <img src={boostrapIcon} alt="bootstrap" />
-            </ScrollAnimation>
-          </div>
-        </div> */}
+
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          delay={0.2 * 1000}
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <p>
+            I love crafting seamless user experiences, optimizing API payloads,
+            and delivering clean, maintainable code that solves real-world
+            problems.
+          </p>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+            Beyond coding, I'm a storyteller—through photography, videography,
+            and gaming. I also run a freelancing page where I design websites
+            and market products like
+            <p style={{ color: "var(--green)" }}>Cooking Grandma</p>{" "}
+            <p>
+              You can also check out my work on{" "}
+              <a
+                href="https://www.instagram.com/buil_dmybrand?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "var(--green)", fontWeight: "bold" }}
+              >
+                Build My Brand
+              </a>
+              .
+            </p>
+          </p>
+        </ScrollAnimation>
       </div>
+
       <div className="about-image">
         <ScrollAnimation animateIn="fadeInRight" delay={0.21 * 1000}>
-          <img src={VinayakSingh} alt="Vinayak Singh" />
+          <img src={VinayakSingh} alt="Aarif Hussain" />
         </ScrollAnimation>
       </div>
     </Container>
-  )
+  );
 }

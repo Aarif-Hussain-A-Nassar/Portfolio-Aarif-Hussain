316d76c05feaa34f118999e4ecf15e3268b4e733
Aarif Hussain <aarifhuss70349@gmail.com>
2025-09-25 09:21:45 +0530

    Skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,15 +4,34 @@ import Marquee from "react-fast-marquee";
 import python from "../../assets/python.svg";
 import java from "../../assets/java.svg";
 import wordpress from "../../assets/wordpress.svg";
-import shopify from "../../assets/shopify.svg";
+import agile from "../../assets/agile.svg";
 import htmlIcon from "../../assets/html-icon.svg";
 import cssIcon from "../../assets/css-icon.svg";
 import jsIcon from "../../assets/js-icon.svg";
 import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
-import vueIcon from "../../assets/vue-icon.svg";
+import angular from "../../assets/angular.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
-
+import aws from "../../assets/aws.svg";
+import azure from "../../assets/azure.svg";
+import c from "../../assets/c.svg";
+import canva from "../../assets/canva.svg";
+import csharp from "../../assets/csharp.svg";
+import eclipse from "../../assets/eclipse.svg";
+import git from "../../assets/git.svg";
+import githubcopilot from "../../assets/githubcopilot.svg";
+import lightroom from "../../assets/lightroom.svg";
+import linux from "../../assets/linux.svg";
+import markdown from "../../assets/markdown.svg";
+import materialui from "../../assets/materialui.svg";
+import mysql from "../../assets/mysql.svg";
+import nextJS from "../../assets/nextJS.svg";
+import postgresql from "../../assets/postgresql.svg";
+import reactquery from "../../assets/reactquery.svg";
+import tailwind from "../../assets/tailwind.svg";
+import typescript from "../../assets/typescript.svg";
+import vscode from "../../assets/vscode.svg";
+import datadog from "../../assets/datadog.svg";
 import { Container } from "./styles";
 
 type Skill = {
@@ -22,14 +41,35 @@ type Skill = {
 
 export function Skills() {
   const skills: Skill[] = [
-    { src: python, alt: "Python" },
+    { src: datadog, alt: "Datadog" },
+    { src: reactquery, alt: "React Query" },
+    { src: vscode, alt: "VS Code" },
+    { src: git, alt: "Git" },
+    { src: githubcopilot, alt: "GitHub Copilot" },
+    { src: linux, alt: "Linux" },
+    { src: canva, alt: "Canva" },
+    // { src: lightroom, alt: "Lightroom" },
+    { src: eclipse, alt: "Eclipse" },
+    { src: c, alt: "C" },
+    { src: csharp, alt: "C#" },
+    { src: mysql, alt: "MySQL" },
+    { src: postgresql, alt: "PostgreSQL" },
+    // { src: sqlite, alt: "SQLite" },
+    { src: nextJS, alt: "Next.js" },
+    { src: tailwind, alt: "Tailwind CSS" },
+    { src: materialui, alt: "Material UI" },
+    { src: typescript, alt: "TypeScript" },
+    { src: aws, alt: "AWS" },
+    { src: azure, alt: "Azure" },
+    { src: angular, alt: "Angular" },
+    { src: agile, alt: "Agile" },
+    { src: markdown, alt: "Markdown" },
+    // { src: python, alt: "Python" },
     { src: java, alt: "Java" },
     { src: jsIcon, alt: "JavaScript" },
     { src: reactIcon, alt: "React" },
     { src: typescriptIcon, alt: "TypeScript" },
-    { src: vueIcon, alt: "Vue" },
     { src: wordpress, alt: "WordPress" },
-    { src: shopify, alt: "Shopify" },
     { src: htmlIcon, alt: "HTML" },
     { src: cssIcon, alt: "CSS" },
     { src: boostrapIcon, alt: "Bootstrap" },
@@ -39,8 +79,8 @@ export function Skills() {
     <Container id="skills">
       <h2>Skills</h2>
       <Marquee
-        gradient={false}   // remove edge fade
-        speed={60}         // adjust speed (lower = slower)
+        gradient={false} // remove edge fade
+        speed={80} // adjust speed (lower = slower)
         pauseOnHover={true}
         pauseOnClick={true}
         autoFill={true}
